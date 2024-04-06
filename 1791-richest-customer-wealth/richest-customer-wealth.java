2f90e1ad997481e729a517be9424548bf4d941be
121910314013 <69963328+121910314013@users.noreply.github.com>
2024-04-07 04:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (30.31%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,15 +1,16 @@
 class Solution {
-    public int maximumWealth(int[][] n) {
-        int a=0;
-        for (int i = 0; i < n.length; i++) {
-            int b=0;
-            for (int j = 0; j < n[0].length; j++) {
-                b+=n[i][j];
+    public int maximumWealth(int[][] arr) {
+        int res=0;
+        for (int row = 0; row < arr.length; row++) {
+            int sum=0;
+            for (int col = 0; col < arr[row].length; col++) {
+                sum+=arr[row][col];
             }
-            if(a<b){
-                a=b;
+            if (res<sum){
+                res=sum;
             }
         }
-        return a;
+        return res;
+        
     }
 }
